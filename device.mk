d74b6c4da49bd5acab67f10e889190d2749eb1a4
mylove90 <engm.saad1990@gmail.com>
2024-07-31 12:08:41 +0000

    Sync changes from A245FXXU5CXE1

--- device.mk
+++ device.mk
@@ -1,9 +1,15 @@
-LOCAL_PATH := device/samsung/a24
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+LOCAL_PATH := device/samsung/a24
 
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+PRODUCT_PACKAGES += shrink
