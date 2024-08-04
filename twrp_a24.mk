cfc62409411118db3a1a3f2b0de46375ee9f2ad3
YZBruh <yagizzengin73@gmail.com>
2024-08-04 18:47:49 +0300

    a24: Make general corrections and edits

--- twrp_a24.mk
+++ twrp_a24.mk
@@ -1,13 +1,6 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -19,14 +12,9 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/a24/device.mk)
 
 PRODUCT_DEVICE := a24
-PRODUCT_NAME := omni_a24
+PRODUCT_NAME := twrp_a24
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A245F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a24nsxx-user 12 SP1A.210812.016 A245FXXU5CXE1 release-keys"
-
-BUILD_FINGERPRINT := samsung/a24nsxx/a24:12/SP1A.210812.016/A245FXXU5CXE1:user/release-keys
